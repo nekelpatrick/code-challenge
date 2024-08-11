63520d8c6ff790d2c79b901cb3dc68c21c3f8f28
nekelpatrick <nekelpatrick@gmail.com>
2024-08-11 16:06:33 -0300

    fix(summary): ensure reactive update of event summary for checked-in and checked-out people

--- ui/components/Summary.js
+++ ui/components/Summary.js
@@ -6,31 +6,35 @@ import { Meteor } from 'meteor/meteor';
 export const Summary = ({ selectedEvent }) => {
   const summary = useTracker(() => {
     if (selectedEvent) {
-      const handler = Meteor.subscribe('people', selectedEvent._id);
-      if (handler.ready()) {
-        const peopleData = People.find({
-          communityId: selectedEvent._id,
-        }).fetch();
+      Meteor.subscribe('people', selectedEvent._id);
 
-        const peopleCheckedIn = peopleData.filter(
-          (person) => person.checkInDate && !person.checkOutDate
-        ).length;
+      // Fetch the people data associated with the selected event
+      const peopleData = People.find({
+        communityId: selectedEvent._id,
+      }).fetch();
 
-        const peopleNotCheckedIn = peopleData.length - peopleCheckedIn;
+      // Calculate the number of people currently checked in
+      const peopleCheckedIn = peopleData.filter(
+        (person) => person.checkInDate && !person.checkOutDate
+      ).length;
 
-        const peopleByCompany = peopleData.reduce((acc, person) => {
-          if (person.checkInDate && !person.checkOutDate) {
-            acc[person.companyName] = (acc[person.companyName] || 0) + 1;
-          }
-          return acc;
-        }, {});
+      // Calculate the number of people not checked in
+      const peopleNotCheckedIn = peopleData.length - peopleCheckedIn;
 
-        return {
-          peopleCheckedIn,
-          peopleByCompany,
-          peopleNotCheckedIn,
-        };
-      }
+      // Group people by company who are currently checked in
+      const peopleByCompany = peopleData.reduce((acc, person) => {
+        if (person.checkInDate && !person.checkOutDate) {
+          const company = person.companyName || 'N/A';
+          acc[company] = (acc[company] || 0) + 1;
+        }
+        return acc;
+      }, {});
+
+      return {
+        peopleCheckedIn,
+        peopleByCompany,
+        peopleNotCheckedIn,
+      };
     }
     return {
       peopleCheckedIn: 0,
@@ -39,9 +43,10 @@ export const Summary = ({ selectedEvent }) => {
     };
   }, [selectedEvent]);
 
+  // Helper function to render the companies and their counts
   const renderCompanies = () =>
     Object.entries(summary.peopleByCompany)
-      .map(([company, count]) => `${company || 'N/A'} (${count})`)
+      .map(([company, count]) => `${company} (${count})`)
       .join(', ');
 
   return (
