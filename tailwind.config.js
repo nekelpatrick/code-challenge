3ea4598d02e6146c0dddc41084b1a19790fec427
nekelpatrick <nekelpatrick@gmail.com>
2024-08-11 16:00:57 -0300

    **fix(tailwind): update content paths to include all JS files in `ui` directory**

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./ui/*.js', './client/*.html'],
+  content: ['./ui/**/*.js', './client/**/*.html'],
   theme: {
     extend: {},
   },
